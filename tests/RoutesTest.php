fbe43cfe4ad8e139ce512042a13031d501168289
James Sessford <hello@jamessessford.com>
2019-10-12 11:23:16 +0100

    Added StyleCI and Travis files. Updated Routes Test. Added Code Coverage packages to --dev

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -7,10 +7,14 @@ class RoutesTest extends TestCase
     /** @test */
     public function is_login_alive()
     {
-        $response = $this->get('/login');
-
-        $response->dump();
-
+        $response = $this->get('login');
         $response->assertSuccessful();
+    }
+
+    /** @test */
+    public function can_i_post_to_login()
+    {
+        $response = $this->post('login');
+        $response->assertStatus(302);
     }
 }
